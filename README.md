b81cbeab41372a53eb69899377fc5e1471a02476
jialeens <jialeadmin@163.com>
2020-08-09 13:18:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+electron版 Linux
---
+++ index.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow, Menu, Tray, Notification } = require('electron');
+let tray = null
+Menu.setApplicationMenu(null);
+
+app.on('ready', () => {
+    tray = new Tray(__dirname + '/dida.png')
+    let win = new BrowserWindow();
+    // win.webContents.openDevTools();
+    var trayMenuTemplate = [
+        {
+            label: '显示/隐藏',
+            click: function () {
+                win.isVisible() ? win.hide() : win.show();
+                win.isVisible() ? win.setSkipTaskbar(false) : win.setSkipTaskbar(true);
+            }
+        },
+        {
+            label: '退出',
+            click: function () {
+                app.quit();
+            }
+        }
+    ];
+    const contextMenu = Menu.buildFromTemplate(trayMenuTemplate);
+    tray.setContextMenu(contextMenu);
+    win.on('close', (event) => {
+        event.preventDefault();
+        win.hide();
+        win.setSkipTaskbar(true);
+    })
+    win.loadURL('https://dida365.com/signin');
+})
